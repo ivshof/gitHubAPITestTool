ea79d6eb11ddcdb993b11a1921226b41afd922e5
vivshin <ivshof@gmail.com>
2021-10-06 10:29:40 +1000

    Test comment3

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     #Test comment
-    #Test comment 2
+    #Test comment 2_12
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
