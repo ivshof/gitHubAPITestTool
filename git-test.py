6102175a3587e236cfda7c0c5520ddb19c5bebcb
vivshin <ivshof@gmail.com>
2021-10-07 15:28:04 +1000

    update git-test.py

--- git-test.py
+++ git-test.py
@@ -5,7 +5,6 @@ from github import Github
 import os
 import base64
 
-
 def main():
 
     envPossibleValues = ['dev', 'sit', 'qa', 'prod']
@@ -23,9 +22,8 @@ def main():
                   'config/topics/gaming/allusers.yaml',
                   'config/topics/gaming/systemusers.yaml']
 
-    envConfiguration = {'rbac': {'dev': 'true', 'sit': 'true', 'qa': 'false', 'prod':'false'},
+    envConfiguration = {'rbac': {'dev': 'false', 'sit': 'true', 'qa': 'false', 'prod':'false'},
                         'topics': {'dev': 'true', 'sit': 'true', 'qa': 'false', 'prod': 'false'}}
-
 
 
     # Ensure correct usage of input parameters
@@ -54,162 +52,87 @@ def main():
     g = Github(apiToken)
     repo = g.get_repo(repoValue)
 
-
-    # repos = g.get_repo(repoValue)
-    # print("repos = g.get_repo(ivshof/gitHabAPITest2")
-    # print(f"repos = {repos}")
-
-
     # Get the latest SHA for scpecidied environment (branch) in case it was not provided by the user
     if commitHash == 'none':
         latestCommit = g.get_repo(repoValue).get_branch(environment)
-
         latestCommitSHA = latestCommit.commit.sha
         commitHash = latestCommitSHA
         print(f"latestCommitSHA = {latestCommitSHA}")
 
-
-    contents = repo.get_contents
-
-    # ==============================
-    # path ='{config/rbac/gaming/allusers.yaml}'
-    #
-    # contents = repo.get_contents("", ref='dev')
-    # while contents:
-    #     file_content = contents.pop(0)
-    #     if file_content.type == "dir":
-    #
-    #         contents.extend(repo.get_contents(file_content.path))
-    #     else:
-    #         print(file_content)
-
-    # for file in contents:
-    #     print(file)
-    # repo.get_contents()
-    contents = repo.get_contents('config/rbac/gaming/systemusers.yaml', ref=environment)
-    #print(f"contents =  {contents}")
-    contents = contents.content
-
-    contents = base64ToString(contents)
-    print(f"contents : \n  {contents}")
-
-
-    #repos = repo.get_commit(sha="ef159d2abe5474f981e5cf8ddf0f75c539054abb")
-    #print(f"repos.commit =  {repos.files}")
-
-    # Get commit hashes for all comits in branch (env)
-    # commits = repo.get_commits(sha=environment)
-    # print(f"comits in {environment} branch:")
-    # for element in commits:
-    #     print(f"element = {element}")
-
-        #
-        # if len(element.files) > 0:
-        #     print(type(element.files[0]))
-        #     print(f"element = {element.files[0]}")
-        #     print(f"element = {element.files}")
-
-    # print(f"commits =  {commits}")
-
-    # Get details about specified commit hash
-    # print(f"CommitSHA = {commitHash}")
+    # Get details for specified commit SHA
     response = repo.get_commit(sha=commitHash).raw_data
-
-    # print("raw data:")
-    # print(response)
-
     parentCommitHash = response['parents'][0]['sha']
-    print(f"parentCommitHash = {parentCommitHash}")
 
     # Check if configuration (yaml) files were modified
-    configFileChanges = []
+    configFileChanged = []
+
+
     for file in range(len(response['files'])):
         fileName = response['files'][file]['filename']
         if filterConfigFileParameter in fileName:
             shaForTheFileName = response['files'][file]['sha']
-            configFileChanges.append({"filename": fileName, "sha": shaForTheFileName})
+            configFileChanged.append({"filename": fileName, "sha": shaForTheFileName})
 
 
-
-    if len(configFileChanges) == 0:
+    if len(configFileChanged) == 0:
         print("No configuration (yaml) files were modified!")
         return
     else:
-        print("Following configuration files have changes ")
-        for filenames in configFileChanges:
-            configFileChanges[filenames]
-
-    # for file in raw_data['files']:
-    #     print(f"FILE =  {raw_data['files'][file]['filename']}")
-
-    # print(type(repos.commit.parents))
-    # print(f'repos.commit.parents = {repos.commit.parents}')
-
-    #print(repos.commit.parents['sha'])
+        print(f"\nFollowing configuration files have changed, Number = {len(configFileChanged)}: ")
+        for filenames in configFileChanged:
+            print(filenames)
 
 
-
+    # Compare provided (or latest commit) with the previous one
     diffCompare = repo.compare(parentCommitHash, commitHash).raw_data
-    # print(f"diffCompare = {diffCompare}")
+
+    print("\nFILES DIFF RESULT")
     for file in range(len(diffCompare['files'])):
         validCheck = 'false'
         fileName = diffCompare['files'][file]['filename']
 
+        #  Check if configuration (yaml) files were modified
         if filterConfigFileParameter in fileName:
-            contents_url = diffCompare['files'][file]['contents_url']
+
             status = diffCompare['files'][file]['status']
-            print(f">>>>> Diff Changes for FILE = {fileName},\nstatus = {status},\ncontents_url={contents_url} \nFor Commits {parentCommitHash} -> {commitHash}")
+            print(f">>>>>\nDiff Changes for FILE = {fileName},\nstatus = {status},\nFor Commits {parentCommitHash} -> {commitHash}")
 
-
+            # Check if file is stored in valud path define in validPaths list
             if fileName in validPaths:
                 print("File location is valid")
                 validCheck = 'true'
-
             else:
                 print("File location is NOT valid")
 
+            # List diff in file content
             if 'patch' in diffCompare['files'][file]:
                 comitDiffText = response['files'][file]['patch']
                 print(f"CHANGES:\n====\n{comitDiffText}\n====\n")
             else:
                 print("CHANGES:\n====\nNo changes in file content\n====\n")
 
-
+            # For validated files get the file content, and print it in case environment is enabled
             if validCheck == 'true':
-                print('validCheck Passed')
+                print('location validCheck Passed')
                 pathList = fileName.split("/")
-                print(pathList)
-                obtainedEngCfg = pathList[1]
-                if obtainedEngCfg in envConfiguration:
-                    if envConfiguration[obtainedEngCfg][environment] == 'true':
-                        print(f"PASS - {envConfiguration[obtainedEngCfg][environment]}")
-                        print(envConfiguration[obtainedEngCfg][environment])
-                        print(f"fileName= {fileName}")
+                obtainedEnvCfg = pathList[1]
 
+                # Check if Env is enabled, and print the file content
+                if obtainedEnvCfg in envConfiguration:
+                    if envConfiguration[obtainedEnvCfg][environment] == 'true':
+                        print(f"Environment check PASSED. {environment} = {envConfiguration[obtainedEnvCfg][environment]}")
+                        print(f"fileName= {fileName} Content:")
+                        contents = repo.get_contents(fileName, ref=environment).content
 
-                        # contents = repo.get_contents(str(fileName))
-                        # print(f"contents = {contents}")
-
+                        #decode from base64 to string
+                        contents = base64ToString(contents)
+                        print(contents)
 
                     else:
-                        print(f"NOT PASS - {envConfiguration[obtainedEngCfg][environment]}")
+                        print(f"Environment check NOT PASSED . {environment} = {envConfiguration[obtainedEnvCfg][environment]}")
 
+            print("<<<<<\n")
 
-
-            print("<<<<<")
-
-
-
-    # for element in range(len(configFileChanges)):
-    #     print(configFileChanges[element]['filename'])
-    #     print(commitHash)
-    #     #print(configFileChanges[element]['sha'])
-    #
-    #     comitToCompare = configFileChanges[element]['sha']
-    #
-    #     diffCompare= repo.compare(str(commitHash), str(comitToCompare))
-    #     print(diffCompare.diff_url)
-    #     print(type(diffCompare.files))
 
 def base64ToString(text):
     return base64.b64decode(text).decode('utf-8')
